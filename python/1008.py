94e36fb3ef5a3cd6afcb1784dc6807b82ce31ab0
gabriel <gabrielcaetanodm@gmail.com>
2019-08-18 15:58:18 -0300

    exercicios python iniciantes

---
+++ python/1008.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+number = int(input())
+hour = int(input())
+value = float(input())
+
+result = (hour * value)
+
+print("NUMBER =", number)
+print("SALARY = U$ %.2f" %result)
